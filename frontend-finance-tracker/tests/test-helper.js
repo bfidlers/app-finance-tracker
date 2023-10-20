40c0a962d0f9162af0bff2ca93e9908eda528c5a
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-20 09:33:07 +0200

    add ember frontend

---
+++ frontend-finance-tracker/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'frontend-finance-tracker/app';
+import config from 'frontend-finance-tracker/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
