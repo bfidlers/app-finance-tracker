da329fbd9bca926b899394f9de50f23504903c88
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-20 10:24:29 +0200

    add registration to mu-project

--- app-finance-tracker/config/dispatcher/dispatcher.ex
+++ app-finance-tracker/config/dispatcher/dispatcher.ex
@@ -21,6 +21,11 @@ defmodule Dispatcher do
   # Run `docker-compose restart dispatcher` after updating
   # this file.
 
+  match "/accounts/*path", @any do
+    Proxy.forward conn, path, "http://registration/accounts/"
+  end
+
+
   match "/*_", %{ layer: :not_found } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
