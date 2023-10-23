38bbca26cab4ea491d597830ad59b390984b3023
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-23 09:35:37 +0200

    make login page inaccessible if logged in

--- frontend-finance-tracker/app/routes/login.js
+++ frontend-finance-tracker/app/routes/login.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class LoginRoute extends Route {}
+
+export default class LoginRoute extends Route {
+    @service session;
+
+    beforeModel(transition) {
+        this.get('session').prohibitAuthentication('index');
+    }
+}
