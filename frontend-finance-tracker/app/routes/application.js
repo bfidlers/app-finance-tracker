f0456c1c394bd45ab88fe8fac0b58374b3703d8e
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-23 09:02:56 +0200

    linter

--- frontend-finance-tracker/app/routes/application.js
+++ frontend-finance-tracker/app/routes/application.js
@@ -2,9 +2,9 @@ import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-    @service session;
+  @service session;
 
-    beforeModel() {
-        return this.session.setup();
-    }
+  beforeModel() {
+    return this.session.setup();
+  }
 }
