c65d052dea10ec20b52d367f1c34f9ce55720699
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-25 11:01:55 +0200

    add relations between user and user account

---
+++ frontend-finance-tracker/app/routes/authenticated/settings.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class SettingsRoute extends Route {
+  @service store;
+  @service session;
+
+  model() {
+    let id = this.session.data.authenticated.data.relationships.account.data.id;
+    return this.store.findRecord('useraccount', id);
+  }
+}
