4247c107915be15b59ffde01888f630c6db3745c
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-25 14:28:58 +0200

    Add ability to change user settings

--- frontend-finance-tracker/app/models/user.js
+++ frontend-finance-tracker/app/models/user.js
@@ -2,8 +2,9 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @attr('string') name;
-  @attr('date') created;
-  @attr('date') modified;
+  @attr('string') created;
+  @attr('string') modified;
+  @attr('string') birthdate;
 
   @hasMany('useraccount', { async: true, inverse: 'user' }) accounts;
 }
