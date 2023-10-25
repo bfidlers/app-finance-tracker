4d8994b0bcebd3b58794f301d2eb66156130cae2
Ben Fidlers <ben.fidlers@qmino.com>
2023-10-25 08:52:29 +0200

    linter

--- frontend-finance-tracker/app/controllers/authors.js
+++ frontend-finance-tracker/app/controllers/authors.js
@@ -4,27 +4,27 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class AuthorsController extends Controller {
-    @tracked newAuthor = '';
+  @tracked newAuthor = '';
 
-    @service store;
+  @service store;
 
-    @action
-    addAuthor(event) {
-        event.preventDefault();
+  @action
+  addAuthor(event) {
+    event.preventDefault();
 
-        //create the author
-        let author = this.store.createRecord('author', {
-            name: this.newAuthor,
-        });
-        author.save();
+    //create the author
+    let author = this.store.createRecord('author', {
+      name: this.newAuthor,
+    });
+    author.save();
 
-        // clear the input fields
-        this.newAuthor = '';
-    }
+    // clear the input fields
+    this.newAuthor = '';
+  }
 
-    @action
-    removeAuthor(author, event) {
-        event.preventDefault();
-        author.destroyRecord();
-    }
+  @action
+  removeAuthor(author, event) {
+    event.preventDefault();
+    author.destroyRecord();
+  }
 }
--- frontend-finance-tracker/app/routes/expenses.js
+++ frontend-finance-tracker/app/routes/expenses.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class ExpensesRoute extends Route {
-    @service store;
+  @service store;
 
-    model() {
-        return this.store.findAll('expense');
-    }
+  model() {
+    return this.store.findAll('expense');
+  }
 }
